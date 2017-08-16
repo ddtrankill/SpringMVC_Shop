7b689951fb20a035960489c97c641f96f878e1b6
ddtrankill <ddtrankill@gmail.com>
2017-08-17 00:27:47 +0300

    Changed xml db config to java config, removed config properties to file

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ With time plan to add more features.
 ## Launch
 To launch it:
 - create database shop,
+- change database properties in '/src/main/resources/database.properties' to your,
 - run on eclipse the code, hibernate creates tables automatically,
 - run in mysql console script 'admin.sql' to add user.
 
--- src/main/java/com/config/AppConfig.java
+++ src/main/java/com/config/AppConfig.java
@@ -1,10 +1,16 @@
 package com.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,7 +20,11 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com")
+@PropertySource(value = { "classpath:database.properties" })
 public class AppConfig extends WebMvcConfigurerAdapter {
+    @Autowired
+    private Environment environment;
+    
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
 
@@ -30,5 +40,15 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("shop");
         return messageSource;
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+        return dataSource;
     }
 }
